55ec9f87ffa61b39e4b4d5ddebfb3c2589dddddb
Adrian Gonzalez <agonzalez@repairpal.com>
2023-05-07 17:10:07 -0700

    add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  # Run only the examples with :focus metadata
+  config.filter_run focus: true
+
+  # Run all examples when everything is filtered
+  config.run_all_when_everything_filtered = true
+
+  # Other configuration options...
+end
